47d0e8454f8f0a1b009a901ddf53941621ab8f90
srikaruio <srikarakula123@gmail.com>
2026-03-25 19:24:00 +0530

    fix build config and stabilize turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,7 @@
 import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* Optimization: Reduce file watching overhead */
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.watchOptions = {
-        ignored: [
-          "**/node_modules/**",
-          "**/.git/**",
-          "**/.next/**",
-          "**/build/**"
-        ],
-        poll: false, // Don't use polling to save CPU
-      };
-    }
-    return config;
-  },
-  // Ensure the cache is handled correctly
-  distDir: ".next",
+  turbopack: {},
 };
 
 export default nextConfig;
